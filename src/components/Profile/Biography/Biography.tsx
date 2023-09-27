acc0f3bbf4e56194ac5ca5dd455f615035472a15
Trongddan <doandan301@gmail.com>
2023-09-27 23:28:29 +0700

    profile2

--- src/components/Profile/Biography/Biography.tsx
+++ src/components/Profile/Biography/Biography.tsx
@@ -5,13 +5,13 @@ export interface Props {}
 
 export default function BioGraphyComponent({}: Props) {
   return (
-    <div className='h-[200px] w-full mt-3 rounded-md p-3 bg-cloud shadow-md'>
+    <div className=' w-full mt-3 rounded-md p-3 bg-cloud shadow-md'>
       <p className='text-base font-semibold'>Giới thiệu</p>
-      <ul className='flex flex-col gap-3 py-2'>
-        <li className='flex items-center gap-3'>
+      <ul className='flex flex-col gap-3 py-2 lg:grid lg:grid-cols-2 lg:gap-3'>
+        <li className='flex items-center gap-3 lg:gap-2'>
           <AiFillHome size={20} color={'#b9b9b9'} />
           <p>
-            Sống tại <span className='font-semibold '>Nam Định</span>
+            Sống tại <span className='font-semibold '>Nam Định,Nam trực</span>
           </p>
         </li>
         <li className='flex items-center gap-3'>
@@ -23,7 +23,7 @@ export default function BioGraphyComponent({}: Props) {
         <li className='flex items-center gap-3'>
           <AiFillHome size={20} color={'#b9b9b9'} />
           <p>
-            Sống tại <span className='font-semibold '>Nam Định</span>
+            Có 105 người theo dõi
           </p>
         </li>
         <li className='flex items-center gap-3'>
